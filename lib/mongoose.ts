147c5103e1d0770993b4d53262a71bffe0488b3e
Nayan Chowdhury <nayan.1aacl@gmail.com>
2025-06-29 09:52:52 +0600

    Reset to previous stage

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -30,37 +30,22 @@ const dbConnect = async (): Promise<Mongoose> => {
   }
 
   if (!cached.promise) {
-    const opts = {
-      dbName: "roadmap",
-      bufferCommands: false, // Disable mongoose buffering
-      maxPoolSize: 10, // Maintain up to 10 socket connections
-      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-      socketTimeoutMS: 5000, // Close sockets after 5 seconds of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
-      maxIdleTimeMS: 30000, // Close connections after 30 seconds of inactivity
-      retryWrites: true, // Retry failed writes
-    };
-
     cached.promise = mongoose
-      .connect(MONGODB_URI, opts)
+      .connect(MONGODB_URI, {
+        dbName: "roadmap",
+      })
       .then((result) => {
         console.log("New connection created");
         return result;
       })
       .catch((err) => {
         console.log("Mongoose connection error", err);
-        cached.promise = null;
         throw err;
       });
   }
 
-  try {
-    cached.conn = await cached.promise;
-    return cached.conn;
-  } catch (error) {
-    cached.promise = null;
-    throw error;
-  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default dbConnect;
